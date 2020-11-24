1ef1718883209534df4697d4229b1429e357f735
trmlnm <filledinanxiety@gmail.com>
2020-11-24 14:06:42 +0100

    add header and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return(
+        <div className="bg-gray-200 text-center text-xs p-3 absolute bottom-0 w-full">
+            © 2020 
+        </div>
+    )
+}
+
+export default Footer
