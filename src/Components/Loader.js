31790b9be4bbc9439acce39973ef45e892f5c07a
trmlnm <filledinanxiety@gmail.com>
2020-11-25 14:07:40 +0100

    add loader component and basic error handling on Product page

---
+++ src/Components/Loader.js
@@ -0,0 +1,12 @@
+import { faDivide } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
+
+function Loader() {
+    return(
+        <div className="flex justify-center">
+            <div className="loader"></div>
+        </div>
+    )
+}
+
+export default Loader;
--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,32 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import Loader from '../Components/Loader'
 
 function Product() {
     const { id } = useParams()
     const url = 'https://5fbe2ffa5923c90016e6aab7.mockapi.io/products/' + id
-    const [product, setProduct] = useState(null)
-
+    const [product, setProduct] = useState({
+        loading: false,
+        data: null,
+        error: false
+    })
     let content = null
 
     useEffect (() => {
-        axios.get(url).then( response => {
-            setProduct(response.data)
+        setProduct({
+            loading: true,
+            data: null,
+            error: false
+        })
+        axios.get(url)
+            .then( response => {
+                setProduct({
+                    loading: false,
+                    data: response.data,
+                    error: false
+                })
+                
+            })
+            .catch(error => {
+                setProduct({
+                loading: false,
+                data: null,
+                error: true
+            })
         })
     }, [url])
     
+    if(product.loading) {
+        content = <Loader />
+    }
 
-    if(product) {
+    if(product.error) {
+        content =  <p> Error, refresh or try again later.</p>
+    }
+
+    if(product.data) {
         content = 
         <div>
             <h1 className="text-2xl font-bold mb-3">
-                {product.name}
+                {product.data.name}
             </h1>
             <div>
-                <img src={product.images} alt={product.name}/>
+                <img src={product.data.images} alt={product.name}/>
             </div>
-    <div className="font-bold text-xl mb-3">$ {product.price}</div>
-    <div className="mb-3">{product.description}</div>
+    <div className="font-bold text-xl mb-3">$ {product.data.price}</div>
+    <div className="mb-3">{product.data.description}</div>
         </div>
     }
     
